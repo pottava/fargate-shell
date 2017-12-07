13fdc4c6032d1f61bb573508935271b18ed28d7c
ryo nakamaru <pottava@gmail.com>
2017-12-07 18:58:59 +0900

    fargate-shell for AWS Cloud9

---
+++ serverless-cloud9/docker/README.md
@@ -0,0 +1,4 @@
+Supported tags and respective `Dockerfile` links:
+
+・latest ([docker/Dockerfile](https://github.com/pottava/fargate-shell/blob/master/serverless-cloud9/docker/Dockerfile))  
+・1.0 ([docker/Dockerfile](https://github.com/pottava/fargate-shell/blob/master/serverless-cloud9/docker/Dockerfile))  
---
+++ serverless-cloud9/docker/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+env | grep AWS_ >> .ssh/environment
+
+if [ "x${SSH_AUTHKEYS_S3_BUCKET}" = "x" ] ; then
+  echo "'SSH_AUTHKEYS_S3_BUCKET' should be specified." 1>&2
+  exit 1
+fi
+if [ "x${SSH_AUTHKEYS_S3_KEY}" = "x" ] ; then
+  echo "'SSH_AUTHKEYS_S3_KEY' should be specified." 1>&2
+  exit 1
+fi
+
+aws sts get-caller-identity
+aws s3api get-object --bucket ${SSH_AUTHKEYS_S3_BUCKET} \
+    --key ${SSH_AUTHKEYS_S3_KEY} .ssh/authorized_keys \
+    > /dev/null
+chown fargate:fargate .ssh/authorized_keys
+chmod 600 .ssh/authorized_keys
+cat .ssh/authorized_keys
+
+# generate host keys if not present
+ssh-keygen -A
+
+# do not detach (-D), log to stderr (-e), passthrough other arguments
+exec /usr/sbin/sshd -D -e "$@"
